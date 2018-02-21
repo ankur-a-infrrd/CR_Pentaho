042589aa8a274cf3889b15d2ff010d35b8facf55
Ankur Agarwal <ankur.a@infrrd.ai>
2018-02-20 18:24:12 -0800

    destination data backup and few cosmetic changes.

--- DataCleanRefreshLoad/DataClean/cust_backup_DDL.sql
+++ DataCleanRefreshLoad/DataClean/cust_backup_DDL.sql
@@ -15,7 +15,7 @@
 /*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-use chrome_expense_etl;
+use chrome_expense_etl_backup_${CUSTOMERID};
 
 CREATE TABLE `tbl_AnalyticsControlOptions` (
   `ID` int(11) NOT NULL AUTO_INCREMENT,
